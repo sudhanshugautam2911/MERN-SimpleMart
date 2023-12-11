65ae050881814f29f2de5a7e4545c9feab75e789
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2023-12-11 21:29:02 +0530

    Big changes in UI and responsiveness

--- controllers/User.js
+++ controllers/User.js
@@ -1,13 +1,22 @@
 const { User } = require("../models/User");
 
 exports.fetchUserById = async (req, res) => {
-  const { id } = req.user;
-  console.log(id)
-  try {
-    const user = await User.findById(id);
-    res.status(200).json({id:user.id,addresses:user.addresses,email:user.email,role:user.role});
-  } catch (err) {
-    res.status(400).json(err);
+  if (req.user) {
+    const { id } = req.user;
+    console.log(id);
+    try {
+      const user = await User.findById(id);
+      res
+        .status(200)
+        .json({
+          id: user.id,
+          addresses: user.addresses,
+          email: user.email,
+          role: user.role,
+        });
+    } catch (err) {
+      res.status(400).json(err);
+    }
   }
 };
 exports.updateUser = async (req, res) => {
